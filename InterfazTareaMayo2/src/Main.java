61087733d10ace4be7d60990ffa1f34a258cba38
moisesmares <124109300+moisesmares@users.noreply.github.com>
2023-04-29 17:03:52 -0600

    Add files via upload

---
+++ InterfazTareaMayo2/src/Main.java
@@ -0,0 +1,8 @@
+import iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Principal ventana = new Principal("Hola Mundo AWT");
+    }
+}
