13830c36f15f9e15a5c0354e5bb4f7d980bf3bbb
moisesmares <124109300+moisesmares@users.noreply.github.com>
2023-05-06 13:31:13 -0600

    Add files via upload

---
+++ LayaoutsTareaTres/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layaouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layaout Cuadricula");
+    }
+}
